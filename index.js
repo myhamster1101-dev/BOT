1da9154c3871b6b9a1cf5ccc46b04356b29b793d
NAO ( นาโอะ ) <myhamster1101@gmail.com>
2026-08-01 11:54:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildMembers
+        GatewayIntentBits.MessageContent
     ]
 });
 
